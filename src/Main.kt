88d9ef7a76c62e4a950505bc32589f4917045404
ses123456789 <sestradasptelo12@gmail.com>
2025-06-01 15:02:22 -0500

    programa terminado y funcional

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,25 @@
+import javax.print.DocFlavor
+
 const val RESPUESTA_AFIRMATIVA = "✓"
 const val RESPUESTA_NEGATIVA = "X"
 const val RESPUESTA_DUDOSA = "?"
 
 //Unimos las respuestas con los valores
 val respuestas = mapOf(
-    "Sí" to RESPUESTA_AFIRMATIVA,
-    "es cierto" to RESPUESTA_AFIRMATIVA,
-    "totalmente" to RESPUESTA_AFIRMATIVA,
-    "en duda" to RESPUESTA_AFIRMATIVA,
-    "Presenta en otro momento" to RESPUESTA_DUDOSA,
-    "No puede decirlo en este momento" to RESPUESTA_DUDOSA,
-    "Puede que si o puede que no" to RESPUESTA_DUDOSA,
-    "No va a suceder" to RESPUESTA_NEGATIVA,
-    "No cuentes con ello" to RESPUESTA_NEGATIVA,
-    "Definitivamente no" to RESPUESTA_NEGATIVA,
-    "No lo creo" to RESPUESTA_NEGATIVA
+    "Yes" to RESPUESTA_AFIRMATIVA,
+    "It's true" to RESPUESTA_AFIRMATIVA,
+    "totally" to RESPUESTA_AFIRMATIVA,
+    "without doubt" to RESPUESTA_AFIRMATIVA,
+    "Try it in another moment" to RESPUESTA_DUDOSA,
+    "I cant tell you that right now" to RESPUESTA_DUDOSA,
+    "Maybe" to RESPUESTA_DUDOSA,
+    "It wont happen" to RESPUESTA_NEGATIVA,
+    "Dont count on that" to RESPUESTA_NEGATIVA,
+    "Definitely not" to RESPUESTA_NEGATIVA,
+    "I dont think so" to RESPUESTA_NEGATIVA
 )//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main(args : Array<string>) {
+fun main() {
 
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
@@ -34,15 +36,39 @@ fun main(args : Array<string>) {
 }
 
 fun mostrarerror() {
-    TODO("Not yet implemented")
+    println("youve picked an invalid option, Try it again")
+    main()
 }
 
 fun salir() {
-    TODO("Not yet implemented")
+    println("See you later")
 }
 
 fun mostrarrespuestas() {
-    println("Select an option \n 1 Show all the answers\n 2 Show only affirmative answers  3 Show only affirmative answers  4 Show only negative answers")
+    println("Select an option \n 1 Show all the answers\n 2 Show only affirmative answers  3 Show only doubtfu answers  4 Show only negative answers")
+    val opcioningresada = readLine()
+    when (opcioningresada){
+        "1" -> mostratrespuestasportipo()
+        "2" -> mostratrespuestasportipo(tipoderespuesta =  RESPUESTA_AFIRMATIVA)
+        "3" -> mostratrespuestasportipo(tipoderespuesta =  RESPUESTA_NEGATIVA)
+        "4" -> mostratrespuestasportipo(tipoderespuesta = RESPUESTA_DUDOSA)
+        else -> println("Invalid option")
+    }
+}
+
+fun mostratrespuestasportipo(tipoderespuesta : String = "todos") {
+when(tipoderespuesta){
+    "todos" -> respuestas.keys.forEach { respuesta -> println(respuesta) }
+
+    RESPUESTA_AFIRMATIVA -> respuestas.filterValues { values -> values == RESPUESTA_AFIRMATIVA }
+        .also { responseTosPositives -> println(responseTosPositives.keys) }
+
+    RESPUESTA_NEGATIVA-> respuestas.filterValues { values -> values == RESPUESTA_NEGATIVA }
+        .also { responseTosNegatives -> println(responseTosNegatives.keys) }
+
+    RESPUESTA_DUDOSA -> respuestas.filterValues { values -> values == RESPUESTA_DUDOSA }
+        .also { responseTosDudosos -> println(responseTosDudosos.keys) }
+}
 }
 
 fun realizarpregunta(){
